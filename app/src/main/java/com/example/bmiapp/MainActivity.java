1ebf371f41d4c668846ce1bc896f3a8f3896f255
Romano Sabbatella <romano_sabbi_1993@hotmail.com>
2022-10-11 20:07:20 +0200

    added first option menu

--- app/src/main/java/com/example/bmiapp/MainActivity.java
+++ app/src/main/java/com/example/bmiapp/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.bmiapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bmiapp.calculate.BmiActivity;
+import com.example.bmiapp.option.OptionActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
     public void openBmiInputActivity(){
         Intent intent = new Intent(this, BmiActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_general, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.menu_general_item_rating:
+                startActivity(new Intent(this, OptionActivity.class));
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 }
