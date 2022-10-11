2e47a225f8878a856404134fea5f8d59ba1f3c33
Romano Sabbatella <romano_sabbi_1993@hotmail.com>
2022-10-11 19:14:49 +0200

    added big chungus

--- app/src/main/java/com/example/bmiapp/option/OptionDetailActivity.java
+++ app/src/main/java/com/example/bmiapp/option/OptionDetailActivity.java
@@ -2,7 +2,10 @@ package com.example.bmiapp.option;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.bmiapp.R;
@@ -22,6 +25,18 @@ public class OptionDetailActivity extends AppCompatActivity {
         Integer index = bundle.getInt("selection_position");
         getTextView(R.id.textViewOptionDetailTitel).setText(BmiCat.GetDescFromIndex(index));
         getTextView(R.id.textViewOptionDetailText).setText(BmiCat.GetDetailDesc(index));
+
+        // Big Chungus
+        if(BmiCat.NORMALRANGE.getDeclaringClass().getEnumConstants().length -1 == index){
+            findViewById(R.id.buttonBigChungus).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.buttonBigChungus).setVisibility(View.GONE);
+        }
+        findViewById(R.id.buttonBigChungus).setOnClickListener(view -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://dbxfanon.fandom.com/wiki/Big_Chungus"));
+            startActivity(browserIntent);
+        });
     }
 
     private TextView getTextView(int id){
